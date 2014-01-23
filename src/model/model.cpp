37638428df233ea1087d9f1baf4a28fa104fc7c4
blaize rhodes <blaize@pobox.com>
2014-01-23 17:19:35 +1000

    working on the state machine in the model

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -1,18 +1,24 @@
 
 #include "model.h"
 
-Model::Model() : CellMap() {
-    //map[new Position(10, 10)] = new Cell(STONE);
+Model::Model() {
+
+    listeners = new std::list<IStateListener*>();
+
 }
-
-
-
-// int const * const * Model::get_map() {
-//     return map;
-// }
-    
 
 void Model::toggle_fullscreen() {
     // FIXME: WE SHOULD GET THIS STATE FROM SDL RATHER THAN DUPLICATE IT!??
     // BUT WE NEED TO SAVE IT?
+}
+
+void Model::change_state(IState * to_state) {
+
+    IState * old_state = current_state;
+    current_state = to_state;
+
+    for(std::list<IStateListener*>::iterator iter = listeners.begin(); iter != listeners.end(); iter++)
+    {
+        (*iter)->state_changed(old_state, current_state);
+    }    
 }
--- src/model/model.h
+++ src/model/model.h
@@ -1,25 +1,51 @@
 #ifndef MODEL_H
 #define MODEL_H
 
-#include <map>
+<list>
 
 #include "cell.h"
 #include "position.h"
 
-typedef std::map<Position, Cell * > CellMap;
+
+class IState {};
+
+class IntroState : IState {
+
+}
+
+class GameState : IState {
+
+}
+
+IModelListener {
+public:
+    //virtual state_changing(IState * from_state, IState * to_state) = 0;
+    virtual state_changed(IState * old_state, IState * current_state) = 0;
+}
+    
+
 
 /**
  * Model for MVC
  *
  */
-class Model : private CellMap {
- private:
-    CellMap map;
+class Model {
+private:
+    IntroState * intro_state;
+    GameState * game_state;
+    
+    IState * current_state;
 
- public: 
+    std::list<IStateListener*> listeners;
+
+public: 
     Model();    
 
     void toggle_fullscreen();
+
+    void change_state(IState * to_state);
+
+
 };
 
 
